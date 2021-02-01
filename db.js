244f9bb4b7336ce80adc9429bb2673dae58445f0
Anatoly <webAnatoly@users.noreply.github.com>
2021-02-01 18:06:10 +0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,67 @@
+{
+  "art_works": [
+    {
+      "id": 1,
+      "title": "Рождение Венеры",
+      "author": "Сандро Боттичелли",
+      "description": "",
+      "price": 1000000,
+      "price_old": 2000000,
+      "currency_code": "USD",
+      "status": "selling",
+      "status_details": "",
+      "images": ["url1"]
+    },
+    {
+      "id": 2,
+      "title": "Тайная вечеря",
+      "author": "Леонардо да Винчи",
+      "description": "",
+      "price": 3000000,
+      "price_old": 0,
+      "currency_code": "USD",
+      "status": "selling",
+      "status_details": "",
+      "images": ["url1"]
+    },
+    {
+      "id": 3,
+      "title": "Сотворение Адама",
+      "author": "Микеланджело",
+      "description": "",
+      "price": 5000000,
+      "price_old": 6000000,
+      "currency_code": "USD",
+      "status": "selling",
+      "status_details": "",
+      "images": ["url1"]
+    },
+    {
+      "id": 4,
+      "title": "Урок анатомии",
+      "author": "Рембрандт",
+      "description": "",
+      "price": 5000000,
+      "price_old": 6000000,
+      "currency_code": "USD",
+      "status": "sold",
+      "status_details": "Продана на аукционе",
+      "images": ["url1"]
+    }
+  ],
+  "comments": [
+    {
+      "id": 1,
+      "body": "some comment",
+      "postId": 1
+    },
+    {
+      "id": 2,
+      "body": "some comment",
+      "postId": 1
+    }
+  ],
+  "profile": {
+    "name": "typicode"
+  }
+}
